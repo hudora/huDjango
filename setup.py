b0fd5282f00fa550db360f42c9227730c1589004
m.dornseif@hudora.de <m.dornseif@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2008-02-28 20:18:33 +0000

    setupscript added
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/huDjango/trunk@3215 2a7d6d32-97f8-0310-8fd3-c67d3a265405

---
+++ setup.py
@@ -0,0 +1,25 @@
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
+
+# patch distutils if it can't cope with the "classifiers" or
+# "download_url" keywords
+from sys import version
+if version < '2.2.3':
+    from distutils.dist import DistributionMetadata
+    DistributionMetadata.classifiers = None
+    DistributionMetadata.download_url = None
+            
+hubarcode = setup(name='huDjango',
+      maintainer='Maximillian Dornseif',
+      maintainer_email='md@hudora.de',
+      url='https://cybernetics.hudora.biz/projects/wiki/huDjango',
+      version='0.7',
+      description='various snippets for use with Django',
+      classifiers=['License :: OSI Approved :: BSD License',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: Python'],
+      # download_url
+      packages=['hudjango'],
+)
+
