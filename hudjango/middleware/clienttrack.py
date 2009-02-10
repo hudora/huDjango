af845e207c94aac75243522a0f9d3381d31b6c3e
m.dornseif@hudora.de <m.dornseif@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2009-02-10 22:30:33 +0000

    implementation of clinet tracking. see http://reviewboard.hudora.biz/r/226/
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/huDjango/trunk@4942 2a7d6d32-97f8-0310-8fd3-c67d3a265405

---
+++ hudjango/middleware/clienttrack.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+middleware.py tracks clients by setting a persistent unique coockie.
+
+You can access it via request.clienttrack_uid in your views.
+There is also request.clienttrack_first_visit and request.clienttrack_first_visit
+available to your views. See ClientTrackMiddleware for details.
+
+Always keep in mind that Coockies can easily be faked.
+
+This has no dependencies on django.contrib.auth and django.contrib.sessions.
+
+Created by Maximillian Dornseif on 2009-02-07.
+Copyright (c) 2009 HUDORA. All rights reserved.
+"""
+
+import time
+import base64
+import hashlib
+import random
+from django.utils.http import cookie_date
+
+
+class ClientTrackMiddleware(object):
+    """The ClientTrackMiddleware tracks Clients (Browsers) by setting a unique coockie.
+    
+    Assuming cookies work as advertised you can access the following variables in yout view functions:
+    
+    * request.clienttrack_uid (always set) - unique ID we assigned to the client.
+      Keep in mind that this is trival to fake.
+    * request.clienttrack_first_visit - timt.time() value at the very first time the client accessed our
+      server or None for first time visitors.
+    * request.clienttrack_last_visit - timt.time() value at the very time the client last accessed our
+      server or None.
+    """
+    
+    def process_request(self, request):
+        if '_hda' in request.COOKIES:
+            parts = request.COOKIES.get('_hda').split(',')
+            request.clienttrack_first_visit, request.clienttrack_uid \
+                = parts[:2]
+            if len(parts) > 3:
+                request.clienttrack_last_visit = parts[2]
+            else:
+                request.clienttrack_last_visit = None                
+        else:
+            request.clienttrack_uid = base64.b32encode(hashlib.md5("%f-%f" % (random.random(), time.time())
+                                                                   ).digest()).rstrip('=')
+            request.clienttrack_last_visit = request.clienttrack_first_visit = None
+    
+    def process_response(self, request, response):
+        if (not getattr('clienttrack_prohibit', request, False)) or not request.clienttrack_first_visit:
+            # even if clienttrack_prohibit is True, we we set the cookie for first time visitors. 
+            if not request.clienttrack_first_visit:
+                request.clienttrack_first_visit = time.time()
+            max_age = 3*365*24*60*60  # 3 years
+            expires_time = time.time() + max_age
+            expires = cookie_date(expires_time)
+            response.set_cookie('_hda', "%d,%s,%f" % (request.clienttrack_first_visit, request.clienttrack_uid, time.time()),
+                                max_age=max_age, expires=expires)
+        return response
