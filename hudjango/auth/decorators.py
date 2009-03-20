1f8fa71a7191645840339157fe5f15a10d381c85
j.diemer@hudora.de <j.diemer@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2009-03-20 15:47:29 +0000

    MøPl + Paketeingang:  * create a hudora base template for django admin panel + app view    * TODO: This base template should be moves into huDjango?  * use django messages  * ModelAdmin: redirect to own "add new item" view MøPl:  * model: move html code into template  * moves staff_member_required from view into urls  * redirect into admin panel after save a screenshot
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/huDjango/trunk@5296 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- hudjango/auth/decorators.py
+++ hudjango/auth/decorators.py
@@ -15,26 +15,27 @@ def require_login(func):
     """This decorator logs in a user via HTTP-Auth."""
     @wraps(func)
     def _decorator(request, *args, **kwargs):
-        if request.META.has_key('HTTP_AUTHORIZATION'):
-            authmeth, auth = request.META['HTTP_AUTHORIZATION'].split(' ', 1)
-            if authmeth.lower() == 'basic':
-                auth = auth.strip().decode('base64')
-                username, password = auth.split(':', 1)
-                user = authenticate(username=username, password=password)
-                if user:
-                    if user.is_active:
-                        login(request, user)
-                        return func(request, *args, **kwargs)
-                    else:
-                        return HttpResponse('disabled account', status=403)
-                else:
-                    response = HttpResponse('invalid login', status=401)
-                    response['WWW-Authenticate'] = 'Basic realm="HUDORA Internal"'
-                    return response
-            else:
-                return HttpResponse('unknown authentication method', status=406)
-        else:
+        if not request.META.has_key('HTTP_AUTHORIZATION'):
             response = HttpResponse('Authentication required', status=401)
             response['WWW-Authenticate'] = 'Basic realm="HUDORA Internal"'
             return response
+            
+        authmeth, auth = request.META['HTTP_AUTHORIZATION'].split(' ', 1)
+        if authmeth.lower() != 'basic':
+            return HttpResponse('unknown authentication method', status=406)
+
+        auth = auth.strip().decode('base64')
+        username, password = auth.split(':', 1)
+        user = authenticate(username=username, password=password)
+        if not user:
+            response = HttpResponse('invalid login', status=401)
+            response['WWW-Authenticate'] = 'Basic realm="HUDORA Internal"'
+            return response
+            
+        if not user.is_active:
+            return HttpResponse('disabled account', status=403)
+            
+        login(request, user)
+        return func(request, *args, **kwargs)
+
     return _decorator
