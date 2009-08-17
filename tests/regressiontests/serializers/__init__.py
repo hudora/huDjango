598c187cc2122e570dfead5aa38014409cde378e
m.dornseif@hudora.de <m.dornseif@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2009-08-17 21:22:23 +0000

    files i forgot
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/huDjango/trunk@6320 2a7d6d32-97f8-0310-8fd3-c67d3a265405

---
+++ tests/regressiontests/serializers/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+regressiontests/serializers/__init__.py
+
+Created by Maximillian Dornseif on 2009-08-17.
+Copyright (c) 2009 HUDORA. All rights reserved.
+"""
---
+++ tests/regressiontests/serializers/models.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+regressiontests/serializers/models.py
+
+Created by Maximillian Dornseif on 2009-08-17.
+Copyright (c) 2009 HUDORA. All rights reserved.
+"""
+
+from django.db import models
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+    
+    def __unicode__(self):
+        return u"%s the place" % self.name
+    
+    def get_absolute_url(self):
+        return 'http://testserver/place/%d/' % self.id
