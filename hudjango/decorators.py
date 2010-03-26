06f352f3343c9455f09b8a9506d009f530549df3
Christian Klein <c.klein@hudora.de>
2010-03-26 10:10:05 +0100

    ajax_request nach huDjango verschoben
    ajax_request verwendet die Django-Klasse HttpResponse
    und macht damit jedes Projekt, was huTools.decorators importiert,
    abhaengig von django.
    
    Aus diesem Grund wird der Decorator in das Django-spezifische Projekt
    huDjango verschoben

---
+++ hudjango/decorators.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+decorators.py
+
+Created by Christian Klein on 2010-03-26.
+Copyright (c) 2007 HUDORA GmbH. All rights reserved.
+"""
+
+from django.http import HttpResponse
+import simplejson as json
+
+
+# The ajax_request decorator was taken from django-annoying
+# see http://pypi.python.org/pypi/django-annoying/0.7.4
+
+class JsonResponse(HttpResponse):
+    """
+    HttpResponse descendant, which return response with ``application/json`` mimetype.
+    """
+    def __init__(self, data):
+        super(JsonResponse, self).__init__(content=json.dumps(data), mimetype='application/json')
+
+
+def ajax_request(func):
+    """
+    If view returned serializable dict, returns JsonResponse with this dict as content.
+
+    example:
+
+        @ajax_request
+        def my_view(request):
+            news = News.objects.all()
+            news_titles = [entry.title for entry in news]
+            return {'news_titles': news_titles}
+    """
+    @wraps(func)
+    def wrapper(request, *args, **kwargs):
+        response = func(request, *args, **kwargs)
+        if isinstance(response, dict):
+            return JsonResponse(response)
+        else:
+            return response
+    return wrapper
